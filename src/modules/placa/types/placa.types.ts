d1db9b0eb172c73ec061ae051166c506ea6f4225
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-19 15:02:46 -0300

    feat: criando types para as colunas uf_registro, categoria_veiculo, cor_tarja e status_placa

---
+++ src/modules/placa/types/placa.types.ts
@@ -0,0 +1,43 @@
+export type UF =
+  | 'AC'
+  | 'AL'
+  | 'AP'
+  | 'AM'
+  | 'BA'
+  | 'CE'
+  | 'DF'
+  | 'ES'
+  | 'GO'
+  | 'MA'
+  | 'MT'
+  | 'MS'
+  | 'MG'
+  | 'PA'
+  | 'PB'
+  | 'PR'
+  | 'PE'
+  | 'PI'
+  | 'RJ'
+  | 'RN'
+  | 'RS'
+  | 'RO'
+  | 'RR'
+  | 'SC'
+  | 'SP'
+  | 'SE'
+  | 'TO';
+
+export type CategoriaVeiculo =
+  | 'PARTICULAR'
+  | 'COMERCIAL'
+  | 'OFICIAL'
+  | 'ALUGUEL';
+
+export type CorTarja = 'CINZA' | 'VERMELHA' | 'AZUL' | 'VERDE' | 'PRETA';
+
+export type StatusPlaca =
+  | 'ATIVA'
+  | 'INATIVA'
+  | 'ROUBADA'
+  | 'EXTRAVIADA'
+  | 'CANCELADA';
