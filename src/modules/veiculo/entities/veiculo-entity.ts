1869c9a32e06f228d703964cd7c803c23b608cef
luanmvcosta0 <luanmvcosta@gmail.com>
2026-03-19 14:20:09 -0300

    feat: criando entidade de Veiculo

---
+++ src/modules/veiculo/entities/veiculo-entity.ts
@@ -0,0 +1,12 @@
+export class VeiculoEntity {
+  renavam: number;
+  chassi: string;
+  marca: string;
+  modelo: string;
+  versao: string;
+  ano_fabricacao: string;
+  ano_modelo: number;
+  cor_predominante: string;
+  capacidade_passageiros: number;
+  peso_bruto_total: number;
+}
